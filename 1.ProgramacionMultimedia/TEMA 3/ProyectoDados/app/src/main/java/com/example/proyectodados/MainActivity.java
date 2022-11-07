ffb78b9997aef7983728589cefde5e38d0938be0
StarBit23 <jortmorc220022@gmail.com>
2022-11-07 17:00:24 +0100

    7 nov 22

--- 1.ProgramacionMultimedia/TEMA 3/ProyectoDados/app/src/main/java/com/example/proyectodados/MainActivity.java
+++ 1.ProgramacionMultimedia/TEMA 3/ProyectoDados/app/src/main/java/com/example/proyectodados/MainActivity.java
@@ -6,6 +6,8 @@ import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -15,7 +17,12 @@ public class MainActivity extends AppCompatActivity {
     ImageView imgDado2;
     ImageView imgDado3;
     ImageView imgCubilete;
+    int valor1;
+    int valor2;
+    int valor3;
     private int suma;
+    Animation rotate;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,16 +35,18 @@ public class MainActivity extends AppCompatActivity {
         imgDado3 = (ImageView)findViewById(R.id.img_dado3);
         imgCubilete = findViewById(R.id.img_vaso);
         imgCubilete.setOnClickListener(v -> tirarDados(viewMain));
+        rotate = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.rotation);
 
     }
 
     @SuppressLint("SuspiciousIndentation")
     public void tirarDados(View v){
+        imgCubilete.setEnabled(false);
         int milisegundos = 1000;
         int i;
 
         txtResultado.setVisibility(View.INVISIBLE);
-        for (i=1; i<5; i++)
+        for (i=1; i<4; i++)
             programarCuandoTirar(milisegundos*i, v);
             programarCuandoMostrarResultados(milisegundos*i++);
         
@@ -60,9 +69,11 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void mostrarDatos() throws InterruptedException {
-        int valor1 = 1+(int)(Math.random()*6);
-        int valor2 = 1+(int)(Math.random()*6);
-        int valor3 = 1+(int)(Math.random()*6);
+        //Random
+        valor1 = 1+(int)(Math.random()*6);
+        valor2 = 1+(int)(Math.random()*6);
+        valor3 = 1+(int)(Math.random()*6);
+        suma=valor1+valor2+valor3;
         switch (valor1){
             case 1:
                 imgDado1.setImageResource(R.drawable.dado1);break;
@@ -107,8 +118,9 @@ public class MainActivity extends AppCompatActivity {
             case 6:
                 imgDado3.setImageResource(R.drawable.dado6);break;
         }
-
-        suma=valor1+valor2+valor3;
+        imgDado1.startAnimation(rotate);
+        imgDado2.startAnimation(rotate);
+        imgDado3.startAnimation(rotate);
     }
 
     private void programarCuandoMostrarResultados(int milisegundos){
@@ -122,8 +134,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void mostrarResultado() {
-        txtResultado.setVisibility(View.VISIBLE);
         txtResultado.setText(String.valueOf(suma));
+        txtResultado.setVisibility(View.VISIBLE);
+        imgCubilete.setEnabled(true);
     }
 
 
