9f8e74ec5c07e38e569ba762d3f303d32080359e
Jorge Ortega Morales <jortmorc@g.educaand.es>
2023-02-01 23:36:52 +0100

    1 feb 23 tarde

--- 3.Interfaces/Tema3 - React/ejercicios_react/ejercicio5-6-7/src/index.js
+++ 3.Interfaces/Tema3 - React/ejercicios_react/ejercicio5-6-7/src/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import HotelView from './HotelView';
+import './index.css'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <App />
+  <App>
+    <HotelView/>
+  </App>
 );
 
 reportWebVitals();
