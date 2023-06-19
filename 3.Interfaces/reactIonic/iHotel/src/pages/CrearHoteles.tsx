4ba561f3c0ee7c45e9e5131a315204f273e80cde
Jorge Ortega Morales <jortmorc@g.educaand.es>
2023-06-19 20:41:35 +0200

    19/06

---
+++ 3.Interfaces/reactIonic/iHotel/src/pages/CrearHoteles.tsx
@@ -0,0 +1,49 @@
+const patchComentariosHotel = async (hotel) => {
+    const response = await fetch('http://localhost:3000/listaHoteles/' + hotel.id, {
+      method: 'PATCH',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(hotel)
+    });
+    const data = response.json();
+  }
+
+const HotelList = (props) => {
+
+
+    const saveComentarios = async (hoteles, hotel, comentario, usuario) => {
+      hotel.comentarios = [...hotel.comentarios, {usuario, comentario}];
+      await patchComentariosHotel(hotel);
+      for(let i=0; i < hoteles.length; i++) {
+        if (hoteles[i].id === hotel.id) {
+          hoteles[i].comentarios = hotel.comentarios;
+        }
+      }
+      props.setHoteles([...hoteles]);
+    }
+
+
+
+    return (
+      <ul>
+        {props.hoteles.map((hotel) => 
+        <li key= {hotel.id}> 
+          <h1>{hotel.name}</h1>
+          <h2>Estrellas: {hotel.estrellas}</h2> 
+          <h2>Ciudad: {hotel.ciudad}</h2>
+          <h2>Precio: {hotel.precio}€</h2>
+          <img alt="imagen hotel" style={{"width":"430px", "height":"auto"}} src={hotel.img}/>
+          <div>
+            <h2>Comentario: </h2>
+            <ul >{hotel.comentarios.map((comentario)=><li>{comentario.usuario} - {comentario.comentario}</li>)}</ul> 
+            <input id="nombreComentario" type="text" placeholder="Nombre"></input>
+            <input id="comentario" type="text" placeholder="Escriba un comentario"></input>
+            <button onClick={(e)=>{
+              saveComentarios(props.hoteles, hotel, e.target.previousSibling.value, e.target.previousSibling.previousSibling.value)}}>Enviar</button>
+          </div>
+        </li>)}
+        
+      </ul>
+    );
+};
+
+export default HotelList;
---
+++ 3.Interfaces/reactIonic/iHotel/src/pages/Page.tsx
@@ -0,0 +1,34 @@
+import {IonButtons, IonContent, IonDatetime, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { useParams } from 'react-router';
+import './Page.css';
+
+const Page: React.FC = () => {
+
+  const { name } = useParams<{ name: string; }>();
+
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonButtons slot="start">
+            <IonMenuButton />
+          </IonButtons>
+          <IonTitle>{"Página Principal"}</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+
+      <IonContent fullscreen>
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle size="large">{name}</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        
+        <IonDatetime></IonDatetime>
+
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Page;
